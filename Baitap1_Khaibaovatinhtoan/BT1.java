9a7268df2f7cad75c38c1c105e3b09fd69ef0754
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-02-05 20:24:37 +0700

    Create BT1.java

---
+++ Baitap1_Khaibaovatinhtoan/BT1.java
@@ -0,0 +1,13 @@
+public class BT1{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        double c=3.5;
+        int tong=a+b;
+        double thuong=b/c;
+        int phandu=a;
+        System.out.println("Tong cua a va b la: " + tong);
+        System.out.println("Thuong cua b va c la: " + thuong);
+        System.out .println("Phan du la: " + phandu);
+    }
+}
