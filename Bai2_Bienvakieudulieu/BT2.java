0b47b1024fa52d5319a6d3780ed0db1543196c3f
hoangnd66cntt-droid <hoang.nd.66cntt@ntu.edu.vn>
2026-02-05 23:30:02 +0700

    GiaiBT

---
+++ Bai2_Bienvakieudulieu/BT2.java
@@ -0,0 +1,7 @@
+public class BT2 {
+    public static void main(String[] args) {
+        double c = 25.0;
+        double f = (c*9/5)+32;
+        System.out.println(c + "°C = " + f + "°F");
+    }
+}
